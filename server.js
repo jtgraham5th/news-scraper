8367033cddcdca555c3aa215ae012293cad78edb
jtgraham5th <jtgraham5th@gmail.com>
2019-10-05 20:48:48 -0400

    added models and updated server js to pull from espn.com

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 // Dependencies
 var express = require("express");
-var mongoose = require("mongoose")
-var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+// var mongojs = require("mongojs");
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+var models = require("./models");
 
 // Initialize Express
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 // Database configuration
 // var databaseUrl = "scraper";
 // var collections = ["scrapedData"];
@@ -36,59 +44,47 @@ app.get("/", function (req, res) {
 });
 
 // Retrieve data from the db
-app.get("/all", function (req, res) {
+app.get("/articles", function (req, res) {
   // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function (error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
-
-// Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function (req, res) {
-  // Make a request via axios for the news section of `ycombinator`
-  axios.get("https://news.ycombinator.com/").then(function (response) {
-    // Load the html body from axios into cheerio
-    var $ = cheerio.load(response.data);
-    // For each element with a "title" class
-    $(".title").each(function (i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
-
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link
-        },
-          function (err, inserted) {
-            if (err) {
-              // Log the error if one is encountered during the query
-              console.log(err);
-            }
-            else {
-              // Otherwise, log the inserted data
-              console.log(inserted);
-            }
-          });
-      }
+  models.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
     });
   });
 
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
+  // Scrape data from one site and place it into the mongodb db
+  app.get("/scrape", function (req, res) {
+    // Make a request via axios for the news section of `ycombinator`
+    axios.get("https://www.espn.com/nfl/").then(function (response) {
+      // Load the html body from axios into cheerio
+      var $ = cheerio.load(response.data);
+      // For each article element that has a section and  'a' class
+      $("article section a").each(function (i, element) {
+          // console.log(i);
+          // console.log(element);
+        var result = {}
 
+        result.title = $(this).find(".contentItem__title").text();
+        result.link = $(this).attr("href");
+        console.log(result)
 
-// Listen on port 3000
-app.listen(PORT, function () {
-  console.log("App running on port 8080!", PORT, PORT);
-});
+        models.Article.create(result)
+          .then(function (dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+        });
+        // Send a "Scrape Complete" message to the browser
+        res.send("Scrape Complete");
+      });
+    });
+
+      // Listen on port 3000
+      app.listen(PORT, function () {
+        console.log("App running on port 8080!", PORT, PORT);
+      });
